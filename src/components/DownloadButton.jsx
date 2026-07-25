43e12b1ae1575f14805ca5e6d3cbd301134c1b10
RohithReddy-1402 <r14v18@gmail.com>
2026-07-25 20:11:49 +0530

    route changed

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -21,7 +21,7 @@ export default function DownloadButton({ paper, addToast, getFileDownloadUrl }) 
         const link = document.createElement("a");
 
     link.href =
-    `https://back-6j6v.onrender.com/papers/api/download/${paper.paper_id}`;
+    `https://back-6j6v.onrender.com/api/papers/download/${paper.paper_id}`;
         link.setAttribute("download", `${paper.title} - ${paper.examType}`);
         document.body.appendChild(link);
         link.click();
