5f4cee8b6bb63da5aac25c690e7915d5c7f563e7
Rohith Kumar Reddy <111877972+RohithReddy-1402@users.noreply.github.com>
2026-09-05 21:25:47 +0530

    Update AdSenseAd with new ad client and slot

---
+++ src/components/AdSenseAd.jsx
@@ -0,0 +1,16 @@
+const AdSenseAd = () => {
+  return (
+    <div className="ad-container">
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-4166992993403707"
+        data-ad-slot="3258405459"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      />
+    </div>
+  );
+};
+
+export default AdSenseAd;
